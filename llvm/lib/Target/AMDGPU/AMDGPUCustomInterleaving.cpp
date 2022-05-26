6316d1d9c9043184681f6d0c26afc5cd2f2e67f6
Wen-Heng (Jack) Chung <whchung@gmail.com>
2022-05-26 13:58:03 -0500

    XXX 6

--- llvm/lib/Target/AMDGPU/AMDGPUCustomInterleaving.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUCustomInterleaving.cpp
@@ -26,33 +26,6 @@ public:
   void apply(ScheduleDAGInstrs *DAG) override;
 };
 
-// Try recognize a GEMM hot loop.
-// The 0th SUnit would be an inline asm.
-// The last SUnit would be an S_CBRANCH_SCC1.
-bool identifyGEMMHotLoop(ScheduleDAGInstrs *DAG) {
-  bool gotBegin = false;
-  bool gotEnd = false;
-
-  const SUnit &SU = DAG->SUnits[0];
-  if (SU.isInstr()) {
-    const MachineInstr *MI = SU.getInstr();
-    if (MI->isInlineAsm()) {
-      gotBegin = true;
-    }
-  }
-
-  if (gotBegin) {
-    if (DAG->ExitSU.getInstr() != nullptr) {
-      const MachineInstr *MI = DAG->ExitSU.getInstr();
-      if (MI->getOpcode() == AMDGPU::S_CBRANCH_SCC1) {
-        gotEnd = true;
-      }
-    }
-  }
-
-  return (gotBegin && gotEnd);
-}
-
 static bool isDSRead(const SUnit &SU) {
   MachineInstr *MI = SU.getInstr();
   return (SIInstrInfo::isDS(*MI) && (MI->mayLoad()));
@@ -77,11 +50,48 @@ static bool isVMEMStore(const SUnit &SU) {
   return (SIInstrInfo::isVMEM(*MI) && (MI->mayStore()));
 }
 
+static bool isInlineAsm(const SUnit &SU) {
+  MachineInstr *MI = SU.getInstr();
+  return MI->isInlineAsm();
+}
+
+// Try recognize a GEMM hot loop.
+// The 0th SUnit would be:
+// - CK: an inline asm.
+// - MS benchmark: a VMEM load.
+// The last SUnit would be an S_CBRANCH_SCC1.
+bool identifyGEMMHotLoop(ScheduleDAGInstrs *DAG) {
+  bool gotBegin = false;
+  bool gotEnd = false;
+
+  const SUnit &SU = DAG->SUnits[0];
+  if (SU.isInstr()) {
+    if (isInlineAsm(SU) || isVMEMLoad(SU)) {
+      gotBegin = true;
+    }
+  }
+
+  if (gotBegin) {
+    if (DAG->ExitSU.getInstr() != nullptr) {
+      const MachineInstr *MI = DAG->ExitSU.getInstr();
+      if (MI->getOpcode() == AMDGPU::S_CBRANCH_SCC1) {
+        gotEnd = true;
+      }
+    }
+  }
+
+  return (gotBegin && gotEnd);
+}
+
 void CustomInterleaving::apply(ScheduleDAGInstrs *DAG) {
+#if 1
+  llvm::errs() << "Try identify a GEMM hot loop DAG.\n";
+#endif
+
   if (!identifyGEMMHotLoop(DAG))
     return;
 
-#if 0
+#if 1
   llvm::errs() << "Inside a GEMM hot loop DAG.\n";
 #endif
 
@@ -124,7 +134,7 @@ void CustomInterleaving::apply(ScheduleDAGInstrs *DAG) {
     }
   }
 
-#if 0
+#if 1
   llvm::errs() << "DSRead instruction count: " << DSReadCount << "\n";
   llvm::errs() << "DSWrite instruction count: " << DSWriteCount << "\n";
   llvm::errs() << "VMEMLoad instruction count: " << VMEMLoadCount << "\n";
@@ -134,34 +144,73 @@ void CustomInterleaving::apply(ScheduleDAGInstrs *DAG) {
 
   assert(VMEMStoreCount == 0);
 
+  // Determine the order of interleaving.
+  int64_t DSReadPriority, DSWritePriority, VMEMLoadPriority;
+  DSReadPriority = DSWritePriority = VMEMLoadPriority = -1;
+  auto NotAssignedPriority = [](int64_t prio) { return prio < 0; };
+
+  int64_t CurrentPriority, TotalPriority;
+  CurrentPriority = TotalPriority = 0;
+
+  // Starting backward.
+  int64_t SUIter = DAG->SUnits.size() - 1;
+  while (SUIter >= 0) {
+    SUnit &SU = DAG->SUnits[SUIter--];
+    if (isDSRead(SU) && NotAssignedPriority(DSReadPriority)) {
+      DSReadPriority = CurrentPriority++;
+    } else if (isDSWrite(SU) && NotAssignedPriority(DSWritePriority)) {
+      DSWritePriority = CurrentPriority++;
+    } else if (isVMEMLoad(SU) && NotAssignedPriority(VMEMLoadPriority)) {
+      VMEMLoadPriority = CurrentPriority++;
+    }
+  }
+  TotalPriority = CurrentPriority;
+
+#if 1
+  llvm::errs() << "DSReadPriority: " << DSReadPriority << "\n";
+  llvm::errs() << "DSWritePriority: " << DSWritePriority << "\n";
+  llvm::errs() << "VMEMLoadPriority: " << VMEMLoadPriority << "\n";
+#endif
+
 #if 0
   llvm::errs() << "Add some artificial edges.\n";
 #endif
 
   int64_t MFMAIter = MFMAs.size() - 1;
 
-  // Interleave MFMA with buffer_loads.
-  int64_t VMEMLoadIter = VMEMLoads.size() - 1;
-  while ((VMEMLoadIter >= 0) && (MFMAIter >= 0)) {
-    SUnit* VMEMLoadSU = VMEMLoads[VMEMLoadIter--];
-    SUnit* MFMASU = MFMAs[MFMAIter--];
-    DAG->addEdge(MFMASU, SDep(VMEMLoadSU, SDep::Artificial));
-  }
+  // Reset CurrentPriority.
+  CurrentPriority = 0;
 
-  // Interleave MFMA with ds_writes.
-  int64_t DSWriteIter = DSWrites.size() - 1;
-  while ((DSWriteIter >= 0) && (MFMAIter >= 0)) {
-    SUnit* DSWriteSU = DSWrites[DSWriteIter--];
-    SUnit* MFMASU = MFMAs[MFMAIter--];
-    DAG->addEdge(MFMASU, SDep(DSWriteSU, SDep::Artificial));
-  }
+  // Iterate through all different instruction groups to be interleaved with MFMA.
+  while (CurrentPriority < TotalPriority) {
+    if (CurrentPriority == VMEMLoadPriority) {
+      // Interleave MFMA with buffer_loads.
+      int64_t VMEMLoadIter = VMEMLoads.size() - 1;
+      while ((VMEMLoadIter >= 0) && (MFMAIter >= 0)) {
+        SUnit* VMEMLoadSU = VMEMLoads[VMEMLoadIter--];
+        SUnit* MFMASU = MFMAs[MFMAIter--];
+        DAG->addEdge(MFMASU, SDep(VMEMLoadSU, SDep::Artificial));
+      }
+    } else if (CurrentPriority == DSWritePriority) {
+      // Interleave MFMA with ds_writes.
+      int64_t DSWriteIter = DSWrites.size() - 1;
+      while ((DSWriteIter >= 0) && (MFMAIter >= 0)) {
+        SUnit* DSWriteSU = DSWrites[DSWriteIter--];
+        SUnit* MFMASU = MFMAs[MFMAIter--];
+        DAG->addEdge(MFMASU, SDep(DSWriteSU, SDep::Artificial));
+      }
+    } else if (CurrentPriority == DSReadPriority) {
+      // Interleave MFMA with ds_reads.
+      int64_t DSReadIter = DSReads.size() - 1;
+      while ((DSReadIter >= 0) && (MFMAIter >= 0)) {
+        SUnit* DSReadSU = DSReads[DSReadIter--];
+        SUnit* MFMASU = MFMAs[MFMAIter--];
+        DAG->addEdge(MFMASU, SDep(DSReadSU, SDep::Artificial));
+      }
+    }
 
-  // Interleave MFMA with ds_reads.
-  int64_t DSReadIter = DSReads.size() - 1;
-  while ((DSReadIter >= 0) && (MFMAIter >= 0)) {
-    SUnit* DSReadSU = DSReads[DSReadIter--];
-    SUnit* MFMASU = MFMAs[MFMAIter--];
-    DAG->addEdge(MFMASU, SDep(DSReadSU, SDep::Artificial));
+    // Move to the next instruction groups.
+    ++CurrentPriority;
   }
 
 #if 0
